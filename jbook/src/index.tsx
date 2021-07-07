1b3c6b2846de73539768aca0c62d8dcb036955bf
Kevin <kev.saintigny.schmidt@gmail.com>
2021-07-07 07:17:43 +0200

    started ESBuild

---
+++ jbook/src/index.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
