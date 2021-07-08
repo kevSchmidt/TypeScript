b170e4e12ad523eb4182424cbf464351361a8e99
Kevin <kev.saintigny.schmidt@gmail.com>
2021-07-08 07:26:47 +0200

    transpiling and bundling work

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -28,19 +28,18 @@ const App = () => {
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin()],
+      define: {
+        "process.env.NODE_ENV": `"production"`,
+        global: "window",
+      },
     });
-
-    // console.log(result);
 
     setCode(result.outputFiles[0].text);
   };
 
   return (
     <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
